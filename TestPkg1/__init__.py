654c2dda3b2e8c64eb7d3e0e95eb8d59e18d671b
cartersm <cartersm@rose-hulman.edu>
2016-02-12 11:09:10 -0500

    Add docstrings and error-checking

--- TestPkg1/__init__.py
+++ TestPkg1/__init__.py
@@ -1,15 +1,4 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-
-img = cv2.imread('../images/stranger.jpg')
-b,g,r = cv2.split(img)
-img2 = cv2.merge([r,g,b])
-plt.subplot(121);plt.imshow(img) # expects distorted color
-plt.subplot(122);plt.imshow(img2) # expect true color
-plt.show()
-
-cv2.imshow('bgr image',img) # expects true color
-cv2.imshow('rgb image',img2) # expects distorted color
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+try:
+    from pytesseract import image_to_string
+except ImportError:
+    from pytesseract.pytesseract import image_to_string
